4e6cef4951bb3ef205e12216625bf5a070bda76b
Mohamad Salimi <contact@salimi.my>
2023-05-12 07:17:40 +0800

    NextAuth, Prisma & MongoDB setup

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,63 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt';
+
+import prismadb from '../../../libs/prismadb';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Email',
+          type: 'text'
+        },
+        password: {
+          label: 'Password',
+          type: 'password'
+        }
+      },
+      async authorize(credentials: any) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password required');
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Email does not exist');
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Incorrect password');
+        }
+
+        return user;
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth'
+  },
+  debug: process.env.NODE_ENV == 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+export default NextAuth(authOptions);
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import NetflixLogo from '../components/svg/NetflixLogo';
 import Input from '../components/Input';
+import axios from 'axios';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -14,6 +19,35 @@ const Auth = () => {
       currentVariant == 'login' ? 'register' : 'login'
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -56,7 +90,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className='bg-red-netflix py-3 font-semibold text-white rounded-md w-full mt-10 hover:bg-red-netflix-dark transition'>
+            <button
+              onClick={variant == 'login' ? login : register}
+              className='bg-red-netflix py-3 font-semibold text-white rounded-md w-full mt-10 hover:bg-red-netflix-dark transition'
+            >
               {variant == 'login' ? 'Sign In' : 'Sign Up'}
             </button>
             <p className='text-neutral-500 mt-12'>
